0aabdf04fc1a947fd703eb5038740d5e691c8c91
Pranpriya <s6506021622027@email.kmutnb.ac.th>
2024-03-03 16:33:19 +0700

    t1

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+const path = require("path");
+const app = express();
+var bodyParser = require('body-parser');
+require('dotenv').config();
+const PORT = process.env.PORT || 5500;
+
+const base_url = "http://node59654-env-8965744.proen.app.ruk-com.cloud:11947";
+
+app.set("views",path.join(__dirname,"/public/views"));
+app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",async(req, res) => {
+    try {
+        // const response = await axios.get(base_url + '/books');
+        res.render("login", );
+    } catch (err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+app.get("/create", (req,res) => {
+    res.render("member");
+});
+
+app.post("/create", async (req,res)=>{
+            const response = await axios.post(base_url + '/users',req.body);
+
+            console.log(response);
+})
+
+
+app.listen(PORT, () => {
+    console.log('Sever started on post 5500');
+});
